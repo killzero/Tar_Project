bebec1147fb4a1af68a5a04a2c4e1f41d3deb643
killzero <p.5313@hotmail.com>
2019-02-06 23:54:04 +0700

    add_SDcard

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@ uint8_t moisture_pin[3] = {A5, A6, A7}; // moisture sensor pin
 uint8_t solenoid_pin[3] = {5, 6, 7};	// solenoid pin
 
 #define humidAir_pin A10 // yellow
-#define TempAir_pin A11  // yellow
+#define TempAir_pin A12  // white
 
 uint32_t currentTime = millis(); // timer value
 
@@ -23,10 +23,11 @@ void setup()
 		pinMode(moisture_pin[i], INPUT);  // Declare pinmode of sensor
 		pinMode(solenoid_pin[i], OUTPUT); // Declare pinmode of solenoid
 	}
-	pinMode(humidAir_pin, INPUT);
 	pinMode(TempAir_pin, INPUT);
+	pinMode(humidAir_pin, INPUT);
+	
 
-		delay(1000); // delay to prepare to run
+	delay(1000); // delay to prepare to run
 }
 /*
     When we took the readings from the dry soil, 
@@ -42,38 +43,14 @@ uint8_t _max[3] = {55, 55, 55};
 
 uint16_t moisture[3]; // Declare variable to keep measured value
 void readAir();
+void moistureControl();
 
 void loop()
 {
 	// set timer of work (millisecond)
 	if (millis() - currentTime > 500)
 	{
-		for (int i = 0; i < 2; i++)
-		{
-			moisture[i] = analogRead(moisture_pin[i]); // read sensor
-			// moisture[i] = constrain(moisture[i], 0, 550);   // 550 - 10
-			// moisture[i] = map(moisture[i], 550, 0, 0, 100); // map value to percentage
-			// Serial.print(" : ");
-			// Serial.print(moisture[i]);
-
-			// lcd.clear();
-			// lcd.setCursor(0, i);
-			// lcd.print("Humid :");
-			// lcd.setCursor(9, i);
-			// lcd.print(moisture[i]);
-			// solenoid controlh
-			if (_min[i] > moisture[i])
-			{
-				// digitalWrite(solenoid_pin[i], 1);
-				valveOn[i] = true;
-			}
-			else if (valveOn[i] && moisture[i] > _max[i])
-			{
-				// digitalWrite(solenoid_pin[i], 0);
-				valveOn[i] = false;
-			}
-		}
-
+		readAir();
 		Serial.println();
 		currentTime = millis(); // reset timer
 	}
@@ -83,13 +60,43 @@ void readAir()
 {
 	// Get Temperature
 	uint16_t temperature = analogRead(TempAir_pin);
-	temperature = amt1001_gettemperature(temperature);
+	// temperature = amt1001_gettemperature(temperature);
 	Serial.print(temperature);
 	Serial.print(" : ");
 	// Get Humidity
 	uint16_t humid = analogRead(humidAir_pin);
 	double volt = (double)humid * (5.0 / 1023.0);
 	humid = amt1001_gethumidity(volt);
-	
+
 	Serial.println(humid);
+}
+
+void moistureControl()
+{
+
+	for (int i = 0; i < 2; i++)
+	{
+		moisture[i] = analogRead(moisture_pin[i]); // read sensor
+		// moisture[i] = constrain(moisture[i], 0, 550);   // 550 - 10
+		// moisture[i] = map(moisture[i], 550, 0, 0, 100); // map value to percentage
+		// Serial.print(" : ");
+		// Serial.print(moisture[i]);
+
+		// lcd.clear();
+		// lcd.setCursor(0, i);
+		// lcd.print("Humid :");
+		// lcd.setCursor(9, i);
+		// lcd.print(moisture[i]);
+		// solenoid controlh
+		if (_min[i] > moisture[i])
+		{
+			// digitalWrite(solenoid_pin[i], 1);
+			valveOn[i] = true;
+		}
+		else if (valveOn[i] && moisture[i] > _max[i])
+		{
+			// digitalWrite(solenoid_pin[i], 0);
+			valveOn[i] = false;
+		}
+	}
 }
